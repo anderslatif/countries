6de23be983a34433e80d02dc8a119eac99f88e0f
anderslatif <anderslatif@users.noreply.github.com>
2026-03-10 12:45:45 +0100

    Created the Spring Boot scaffold

---
+++ src/main/java/dk/ek/countries/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package dk.ek.countries.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to the countries website!";
+    }
+}
